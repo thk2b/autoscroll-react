9ce31013112f6820d6d3dcfab3d5e0a65150bbd0
Theo <theokobb@Theos-Air.fios-router.home>
2018-01-03 22:43:17 -0500

    write component

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const isScrolledDown = el => {
+    const bottom = el.scrollTop + el.clientHeight
+    return bottom >= el.scrollHeight - 150
+}
+
+const scrollDown = el => el.scrollTop = el.scrollHeight - el.clientHeight
+
+export default Component => class extends React.Component {
+    constructor(props){
+        super(props)
+        this._hasScrolledUp = false /* whether the user has scrolled up */
+        this._el = null
+    }
+    
+    componentDidUpdate(){
+        if(!this._hasScrolledUp){
+            scrollDown(this._el)
+        }
+    }
+
+    handleScroll(e){
+        if(isScrolledDown(this._el)){
+            this._hasScrolledUp = false
+        } else {
+            this._hasScrolledUp = true
+        }
+    }
+
+    render(){
+        return <Component 
+            {...this.props}
+            ref={ el => this._el = ReactDOM.findDOMNode(el) }
+            onScroll={ e => this.handleScroll(e) }
+        />
+    }
+}
